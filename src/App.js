ee696c82fa4f8d90f7b0a1b907a7e2017ffffbfd
Youngju-Jang <joj1043@nate.com>
2023-06-20 15:50:23 +0900

    [FEAT] 배열 렌더링
    related to #3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
+const dummyList = [
+  {
+    id: 1,
+    author: "hippo",
+    content: "hi 1",
+    emotion: 5,
+    create_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "abc",
+    content: "hi 2",
+    emotion: 4,
+    create_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "mojo",
+    content: "hi 3",
+    emotion: 2,
+    create_date: new Date().getTime(),
+  },
+];
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
